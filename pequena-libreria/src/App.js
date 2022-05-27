49c268e5c192110221df13f188df8fa321231836
Santiago Villalba <santivillabal@gmail.com>
2022-05-27 17:58:31 -0300

    Commit react app

--- pequena-libreria/src/App.js
+++ pequena-libreria/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          La pequeña librería
         </a>
       </header>
     </div>
